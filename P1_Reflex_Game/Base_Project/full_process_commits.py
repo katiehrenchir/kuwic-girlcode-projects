272fb828bd71883dbbf049c57cd35990b4ebdc34
Katie Hrenchir <katie.hrenchir@gmail.com>
2018-09-05 12:21:45 -0500

    P1 base project process commit 4.5

--- P1_Reflex_Game/Base_Project/full_process_commits.py
+++ P1_Reflex_Game/Base_Project/full_process_commits.py
@@ -1,28 +1,24 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 from time import sleep
 from random import uniform
 
+# Declare LED at GPIO 4
 led = LED(4)
 
-while True:
-    led.on()
-    sleep(uniform(1,4))
-    
-    led.off()
-    sleep(uniform(1,4))
-    from gpiozero import LED
-from time import sleep
-from random import uniform
+# Declare buttons at GPIO 14 (top) and 15 (bottom)
+top_button = Button(14)
+bottom_button = Button(15)
 
-led = LED(4)
-
+# Main program
 while True:
-    on_duration = uniform(1,4)
-    print('on for {:f} seconds' .format(on_duration) )
+    top_button.wait_for_press()
+    #on_duration = uniform(1,4)
+    #print('on for {:f} seconds' .format(on_duration) )
     led.on()
-    sleep(on_duration)
+    #sleep(on_duration)
     
-    off_duration = uniform(.5,1)
-    print('off for {:f} seconds' .format(off_duration) )
+    top_button.wait_for_release()
+    #off_duration = uniform(.5,1)
+    #print('off for {:f} seconds' .format(off_duration) )
     led.off()
-    sleep(off_duration)
+    #sleep(off_duration)
