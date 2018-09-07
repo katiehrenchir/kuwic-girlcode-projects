e6f665b43d83ec5fab3c9b763894546b528d70dc
Katie Hrenchir <katie.hrenchir@gmail.com>
2018-09-06 23:35:26 -0500

    P1 history 7

--- P1_Reflex_Game/Base_Project/history.py
+++ P1_Reflex_Game/Base_Project/history.py
@@ -22,7 +22,10 @@ def get_winner_name(button):
 
 # Method to print message when a button has been pressed
 def pressed(button):
-    print(get_winner_name(button) + " has won the game!")
+    if led.value:
+        print(get_winner_name(button) + ' won the game!')
+    else:
+        print('Button was not pressed!')
     
 # Define behavior when either the top or
 # bottom buttons are pressed
@@ -31,13 +34,11 @@ bottom_button.when_pressed = pressed
     
 # Main program
 while True:
-    on_duration = uniform(1,4)
-    print('on for {:f} seconds' .format(on_duration) )
+    on_duration = uniform(.1,.3)
     led.on()
     sleep(on_duration)
     
-    off_duration = uniform(.5,1)
-    print('off for {:f} seconds' .format(off_duration) )
+    off_duration = uniform(5,10)
     led.off()
     sleep(off_duration)
     
