e46a1907b24f3a455055261879ab7dc205816b86
Katie Hrenchir <katie.hrenchir@gmail.com>
2018-09-06 23:34:28 -0500

    P1 history 5

--- P1_Reflex_Game/Base_Project/history.py
+++ P1_Reflex_Game/Base_Project/history.py
@@ -9,17 +9,23 @@ led = LED(4)
 top_button = Button(14)
 bottom_button = Button(15)
 
+# Method to print message when a button has been pressed
+def pressed(button):
+    print("Button has been pressed!")
+    
+# Define behavior when either the top or
+# bottom buttons are pressed
+top_button.when_pressed = pressed
+bottom_button.when_pressed = pressed
+    
 # Main program
 while True:
-    top_button.wait_for_press()
-    #on_duration = uniform(1,4)
-    #print('on for {:f} seconds' .format(on_duration) )
+    on_duration = uniform(1,4)
+    print('on for {:f} seconds' .format(on_duration) )
     led.on()
-    #sleep(on_duration)
+    sleep(on_duration)
     
-    top_button.wait_for_release()
-    #off_duration = uniform(.5,1)
-    #print('off for {:f} seconds' .format(off_duration) )
+    off_duration = uniform(.5,1)
+    print('off for {:f} seconds' .format(off_duration) )
     led.off()
-    #sleep(off_duration)
-    
+    sleep(off_duration)
