e25837f4b20159c60edeefbea80fb155459dc77f
Katie Hrenchir <katie.hrenchir@gmail.com>
2018-09-06 23:33:25 -0500

    P1 history 4

--- P1_Reflex_Game/Base_Project/history.py
+++ P1_Reflex_Game/Base_Project/history.py
@@ -5,8 +5,12 @@ from random import uniform
 led = LED(4)
 
 while True:
+    on_duration = uniform(1,4)
+    print('on for {:f} seconds' .format(on_duration) )
     led.on()
-    sleep(uniform(1,4))
+    sleep(on_duration)
     
+    off_duration = uniform(.5,1)
+    print('off for {:f} seconds' .format(off_duration) )
     led.off()
-    sleep(uniform(1,4))
+    sleep(off_duration)
