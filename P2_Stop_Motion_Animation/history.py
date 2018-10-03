41205652a927f0fe5f0dcbda2f0d5624dd9bccdb
katiehrenchir <katie.hrenchir@gmail.com>
2018-10-02 23:11:32 -0500

    History 04

--- P2_Stop_Motion_Animation/history.py
+++ P2_Stop_Motion_Animation/history.py
@@ -1,10 +1,13 @@
 from picamera import PiCamera
 from time import sleep
+from gpiozero import Button
 
+button = Button(17)
 camera = PiCamera()
 camera.rotation = 180
 
 camera.start_preview()
-sleep(5)
+button.wait_for_press()
+#sleep(5)
 camera.capture('/home/pi/Desktop/image.jpg')
 camera.stop_preview()
