092932a71a4f7ddf9bf7d7c9947daae1fc1e0684
katiehrenchir <katie.hrenchir@gmail.com>
2018-10-02 23:11:47 -0500

    History 05

--- P2_Stop_Motion_Animation/history.py
+++ P2_Stop_Motion_Animation/history.py
@@ -7,7 +7,12 @@ camera = PiCamera()
 camera.rotation = 180
 
 camera.start_preview()
-button.wait_for_press()
-#sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
+
+while True:
+    try:
+        button.wait_for_press()
+        #sleep(5)
+        camera.capture('/home/pi/Desktop/iamge.jpg')
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break
